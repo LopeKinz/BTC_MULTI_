f9b353320807a5b74cb9d2214626d09a2191f080
0x00032 <53447243+0x00032@users.noreply.github.com>
2022-04-02 11:52:51 -0400

    added: Nitro Gen

---
+++ BanHammer TOOLKIT/nitro-gen/main.py
@@ -0,0 +1,29 @@
+#
+# Made By diverse Lisenced To BanHammer
+#
+
+import os
+import string
+import random
+from requests import Session
+from colorama import Fore, init
+
+def clear_console():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+amount = int(input('How Many Codes To Generate: '))
+clear_console()
+print('Generating Codes Now!')
+
+count=0
+for i in range(amount):
+    count+=1
+    code = "https://discordapp.com/gifts/%s" % (('').join(random.choices(string.ascii_letters + string.digits, k=16)))
+    os.system(f'title Count: {count} Current: {code}')
+    with open('codes.txt', 'a') as f:
+        f.write('%s\n' % (code))
+
+os.system(f'title Total Codes Created: {count}')
+clear_console()
+print('Output can be found in \'codes.txt\'')
+input('\n\nPress Enter To Exit...')
