993aee4bc1e5ff5a6c3d4001bacd0ca26b62b326
Sourcery AI <>
2023-05-11 11:53:55 +0000

    'Refactored by Sourcery'

--- BanHammer TOOLKIT/pass-gen/main.py
+++ BanHammer TOOLKIT/pass-gen/main.py
@@ -8,7 +8,6 @@ import random
 def clear_console():
     os.system('cls' if os.name=='nt' else 'clear')
     
-count = 0
 password = []
 
 clear_console()
@@ -16,10 +15,10 @@ length = input("Length (Defualt: 128): ")
 length = 128 if length == '' else int(length)
 clear_console()
 
-while count < length:
+for _ in range(length):
     rand = random.randint (0,3)
     if rand == 0:
-        b = int(random.randint (97,123))
+        b = random.randint(97, 123)
         password.append(b)
     elif rand == 1:
         b = random.randint (65,91)
@@ -38,8 +37,6 @@ while count < length:
         elif r == 2:
             b = random.randint (123,126)
             password.append(b)
-    count += 1
-
 word = "".join([chr(c) for c in password])
 
 print(word)
